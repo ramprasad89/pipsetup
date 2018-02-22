dbc557ea37bc3d0ab225da3999812cb86cf8e83a
Ram Prasad Panda <ram082.iacr@gmail.com>
2018-02-22 19:56:39 +0530

    asdasd

--- setup.py
+++ setup.py
@@ -12,6 +12,12 @@ setup (
 	install_requires=['boto3', 'awscli'],
 	license='GNU',
 	name='helloworld',
+	description='hello world python script for testing python packaging',
+	url='https://github.com/ramprasad89/pipsetup',
 	packages=find_packages(),
 	version=1.0,
+	project_urls={
+		'Reference-repo': 'https://github.com/pypa/sampleproject',
+		'Reference-web': 'https://packaging.python.org/tutorials/distributing-packages/',
+	},
 )
